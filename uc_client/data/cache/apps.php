1e1bb1dbd0cbc3601fbee70c46577996effd3869
yiilover <yiilover@gmail.com>
2014-05-14 21:50:44 +0800

    uc

---
+++ uc_client/data/cache/apps.php
@@ -0,0 +1,54 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz!',
+    'url' => 'http://bbs.ddecshop.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+    'extra' => '',
+    'recvnote' => '1',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'ECSHOP',
+    'name' => '多多模板之家',
+    'url' => 'http://www.ddecshop.com',
+    'ip' => '127.0.0.1',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  3 => 
+  array (
+    'appid' => '3',
+    'type' => 'ECSHOP',
+    'name' => '乐影软件',
+    'url' => 'http://ddecshop.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  'UC_API' => 'http://ucenter.ddecshop.com',
+);
