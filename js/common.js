903962a7b70e1c093db24cf61ed1bfaa20c0010d
balack.xu <balack.xu@b5m.com>
2014-05-20 17:49:07 +0800

    fix

--- js/common.js
+++ js/common.js
@@ -5,3 +5,24 @@ $(function() {
     });
     //$().find('img').trigger('lazyload');
 });
+function addfavorite(obj,url,title) {
+    !url ? url = location.href : null;
+    !title ? title = document.title : null;
+    try{
+        window.external.addFavorite(url, title);
+        return false;
+    }catch(e){
+        try{
+            window.sidebar.addPanel(title, url, "");
+            return false;
+        }catch(e){
+            alert("加入收藏失败，请使用Ctrl+D进行添加");
+            if(location.href.toLowerCase().indexOf(obj.href.toLowerCase(),0)>=0){return false;}
+        }
+    }
+}
+
+var qqLoginWin;
+function head_qq_login(){
+    qqLoginWin = window.open("/qq/login.php","TencentLogin","width=450,height=320,menubar=0,scrollbars=1, resizable=1,status=1,titlebar=0,toolbar=0,location=1");
+}
