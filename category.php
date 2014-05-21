10f938dbc8d3695807cc87e98309e7410faac000
yiilover <yiilover@gmail.com>
2014-05-21 09:44:28 +0800

    fix

--- category.php
+++ category.php
@@ -518,7 +518,9 @@ function category_get_goods($children, $brand, $min, $max, $ext, $size, $page, $
         $arr[$row['goods_id']]['type']             = $row['goods_type'];
         $arr[$row['goods_id']]['promote_price']    = ($promote_price > 0) ? price_format($promote_price) : '';
         $arr[$row['goods_id']]['goods_thumb']      = get_image_path($row['goods_id'], $row['goods_thumb'], true);
-        $arr[$row['goods_id']]['goods_img']        = get_image_path($row['goods_id'], $row['goods_img']);
+        $goods_img        = get_image_path($row['goods_id'], $row['goods_img']);
+        if(strpos($goods_img,'http')===false) $goods_img='/'.$goods_img;
+        $arr[$row['goods_id']]['goods_img']        = $goods_img;
         $arr[$row['goods_id']]['url']              = build_uri('goods', array('gid'=>$row['goods_id']), $row['goods_name']);
     }
 
